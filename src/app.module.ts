f3c3366a56ac6b51cc8995855ee3caeb91df448f
programmerShinobi <shinobiprogrammer@gmail.com>
2024-09-21 00:06:50 +0700

    Load .env using Config Module in NestJS (Series 02)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configLoads } from './modules/config/index.config';
+
+const modules = [];
+
+export const global_modules = [
+  ConfigModule.forRoot({
+    load: configLoads,
+    isGlobal: true,
+    envFilePath: ['.env'],
+  }),
+];
 
 @Module({
-  imports: [],
+  imports: [...global_modules, ...modules],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const log = new Logger(bootstrap.name);
+  // create application instance using NestFactory
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // getting configService from application
+  // to fetch port from app.config.ts config load
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('app.port') || 8000;
+
+  // used the port value here
+  await app.listen(PORT);
+
+  log.log(
+    `NestJS Series (${process.env.npm_package_version}) start on port ${PORT}`,
+  );
 }
 bootstrap();
